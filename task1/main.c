263dd6d4d02b08b577b937ccfa3ee9bc3e30e0fa
Dambarioid <138684532+Dambarioid@users.noreply.github.com>
2024-12-08 12:25:38 -0500

    Add files via upload

---
+++ task1/main.c
@@ -0,0 +1,21 @@
+
+
+/**
+ * main.c -- программа "Hello, students!"
+ *
+ * Copyright (c) 2022, Ivan Cherenkov <dambar2006@gmail.com>
+ *
+ * This code is licensed under MIT license.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    /* Выводим приветствие */
+    fprintf(stdout, "Hello, students!\n");
+
+    return 0;
+}
+
+
