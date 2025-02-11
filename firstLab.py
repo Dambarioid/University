a9598be547535280b61607b88b058866525c6879
Portfolio <sovintomaailmassa@gmail.com>
2025-02-11 09:10:15 +0200

    modified:   firstLab.py

--- firstLab.py
+++ firstLab.py
@@ -1,33 +1,15 @@
-def create_zones(self,x,y,h,w):
-    for zone in range(NumZones):
-        xzone = int(input("Enter the beginning of the zone x: "))
-        yzone = int(input("Enter the beginning of the zone y"))
-        hzone = int(input("Enter the beginning of the zone height"))
-        wzone = int(input("Enter the beginning of the zone weigh"))
-        for i in range(xzone, xzone+wzone):
-            for j in range(yzone, yzone-hzone,-1):
-                zapret.add((i,j))
 class Robot():
 
     height, weight = 100, 100
 
-    def __init__(self,x:int,y:int):
+    def __init__(self,x:int,y:int,forbidden):
         self.x = x
         self.y = y
-        self.zapret_zones = []
+        self.zapret_zones = forbidden
         self.commands = []
         self.saveArr = [(x,y),(x,y)]
-        
 
-    def rem_add(self,back=False):
-        if not back:    
-            self.saveArr.remove(self.saveArr[0])
-            self.saveArr.append((x,y))
-        else:
-            self.saveArr.remove(self.saveArr[1])
-            self.saveArr.append((x,y))
-
-    def _enter_commands(self):
+    def enter_commands(self):
         while True:
             command = input().strip(' ').split(',')
 
@@ -40,102 +22,99 @@ class Robot():
                 break
 
             self.commands.append(command)
+
+    def _rem_add(self,back=False):
+        if not back:    
+            self.saveArr.remove(self.saveArr[0])
+            self.saveArr.append((x,y))
+        else:
+            self.saveArr.remove(self.saveArr[1])
+            self.saveArr.append((x,y))
+
     def _checkCrash(self,steps):
         for x in range(self.x,self.x+steps+1):
             for y in range(self.y,self.y+steps+1):
-                if (x,y) in zapret:
+                if (x,y) in self.zapret_zones:
                     print(f'{x},{y} occured in forbidden zone. Program stopped')
                     exit()
+
     def move(self,directon:str, steps:int) -> str:
         if (((self.x+steps < self.weight) and (-self.weight<self.x-steps)) and ((self.y+steps < self.height) and (-self.height<self.y-steps))):
+
             self._checkCrash(steps=steps)
 
-            if directon  == "R":
+            if directon == "R":
                 [print(i,self.y) for i in range(self.x+1,self.x+steps+1)]
-                x += steps
-                self.rem_add()
+                self.x += steps
+                self._rem_add()
 
-            elif directon  == "L":
-                [print(i,self.y) for i in range(self.x-1,self.x-steps-1,-1)]
-                x -= steps
-                self.rem_add()
+            elif directon == "L":
+                [print(i,self.y) for i in range(self.x-1,self.x-steps-1,-1)]                
+                self.x -= steps
+                self._rem_add()
 
-            elif directon  == "U":
-                [print(self.x,i) for i in range(self.y+1,self.y+steps+1)]
-                y += steps
-                self.rem_add()
 
-            elif directon  == "D":
-                [print(self.x,i) for i in range(self.y-1,self.y-steps-1,-1)]
-                y -= steps
-                self.rem_add()
+            elif directon == "U":
+                [print(self.x,i) for i in range(self.y+1,self.y+steps+1)]                
+                self.y += steps
+                self._rem_add()
+
+            elif directon == "D":
+                [print(self.x,i) for i in range(self.y-1,self.y-steps-1,-1)]                
+                self.y -= steps
+                self._rem_add()
 
             elif directon == "B":
-                for i in range(len(history)):
-                    if history[i][0] == "B":
-                        del history[i]
-                        mustBeCompleted = history[steps:i]
+                for i in range(len(self.commands)):
+                    if self.commands[i][0] == "B":
+                        self.commands[i] = ["",""]
+                        mustBeCompleted = self.commands[i-steps:i]
+                        a = i
+                        print(mustBeCompleted,"-----------_TEst")
                         for j in mustBeCompleted:
-                            j[0] = {"R": "L", "L": "R", "U": "D", "D": "U"}.get(j[0])
-                            func2(j)
-                        
-                        
-
-                if steps % 2 != 0:   
-
-                    if arr[0][0] == x:
-                        if (y < arr[0][1]):
-                            [print(x,i) for i in range(y+1,arr[0][1]+1)]
-                        elif (y > arr[0][1]): 
-                            [print(x,i) for i in range(y-1,arr[0][1]-1,-1)]
-                        y = arr[0][1] 
-                        rem_add(arr,x,y,True)
-                    
-                    elif arr[0][1] == y:
-                        if (x < arr[0][0]):
-                            [print(i,y) for i in range(x+1,arr[0][0]+1)]
-                        elif(x > arr[0][0]):
-                            [print(i,y) for i in range(x-1,arr[0][0]-1,-1)]
-                        x = arr[0][0]
-                        rem_add(arr,x,y,True)
+                            self.commands.insert(a+1,[{"R": "L", "L": "R", "U": "D", "D": "U"}.get(j[0]),j[1]])
+                            a += 1
+                        print(self.commands)
+                        break
+            else:
+                print("You've put unknown command. Program stopped")
+                raise KeyError
         else:
             print("X or Y higher than weigh or height. Program stopped")
 
     def run(self):
-        pass
+        self.enter_commands()
 
-try:
-    x = int(input("Enter the x: "))
-    y = int(input("Enter the y: "))
-except:
-    print("You've input incorrect vars, int type expected")
+        for command in self.commands:
+            self.move(command[0],int(command[1]))
 
+
+def create_zones():
+    try:
+        NumZones = int(input("Enter the number of zones: "))
+        zapret = set()
+        for _ in range(NumZones):
+            print(
+    """
+    Enter the X and Y of the zone and weight and heigh.
+    Remember that you put left up angle of rectangle
+    Example:20 20 100 100: """
+                )
+            zone = input().split(" ")
+            xzone, yzone, hzone, wzone = int(zone[0]),int(zone[1]), int(zone[2]), int(zone[3])
+            for x in range(xzone, xzone+wzone):
+                for y in range(yzone, yzone-hzone,-1):
+                    zapret.add((x,y))   
+        return zapret
+    except:
+        print("You've put something odd. INT type required")       
+         
 if __name__ == "__main__":
-    turtle = Robot(x=x,y=y)
-    turtle.run()
-
-
-print("X and Y are the beginning of the left")
-NumZones = int(input("Enter the number of zones"))
-zapret = set()
-def create_zones(x,y,h,w):
-    for zone in range(NumZones):
-        xzone = int(input("Enter the beginning of the zone x: "))
-        yzone = int(input("Enter the beginning of the zone y"))
-        hzone = int(input("Enter the beginning of the zone height"))
-        wzone = int(input("Enter the beginning of the zone weigh"))
-        for i in range(xzone, xzone+wzone):
-            for j in range(yzone, yzone-hzone,-1):
-                zapret.add((i,j))
-
-
-    
-
-def func():
-    history = []
-    func1(arr=history)
-
-    for command in history:
-        func2(history,saveArr,command[0],int(command[1]))
-
-func()
+    try:
+        zones = create_zones()
+        x = int(input("Enter the x: "))
+        y = int(input("Enter the y: "))
+        turtle = Robot(x=x,y=y,forbidden=zones)
+        turtle.run()
+    except:
+        print("You've input not int type. Program Stopped")
